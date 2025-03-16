cf3c0c290aae042570b1f4ab0ad738685825ec18
Troy Wiegand <troy@troywiegand.com>
2025-03-16 12:55:00 -0400

    Turn Location into struct instead of a string

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
--- main.go
+++ main.go
@@ -9,31 +9,42 @@ import (
     "net/http"
     "time"
     "slices"
+    "fmt"
+    "github.com/joho/godotenv"
 )
 var logger = GetLogger();
-type Location string
-const (
-    Spawn Location = "{\"x\":0,\"y\":0}"
-    Chicken Location = "{\"x\":0,\"y\":1}"
-    CopperMine Location = "{\"x\":2,\"y\":0}"
-    IronMine Location = "{\"x\":1,\"y\":7}"
-    CoalMine Location = "{\"x\":1,\"y\":6}"
-    Forge Location = "{\"x\":1,\"y\":5}"
-    WeaponSmith Location = "{\"x\":2,\"y\":1}"
-    Bank Location = "{\"x\":4,\"y\":1}"
-    AshWood Location = "{\"x\":-1,\"y\":0}"
-    SpruceWood Location = "{\"x\":1,\"y\":9}"
-    BirchWood Location = "{\"x\":-1,\"y\":6}"
-    Sawmill Location = "{\"x\":-2,\"y\":-3}"
-    YellowSlime Location = "{\"x\":4,\"y\":-1}"
-    BlueSlime Location = "{\"x\":2,\"y\":-1}"
-    RedSlime Location = "{\"x\":1,\"y\":-1}"
-    Mushmush Location = "{\"x\":5,\"y\":3}"
-    MonsterTask Location = "{\"x\":1,\"y\":2}"
-    Sunflower Location = "{\"x\":2,\"y\":2}"
-    Alchemist Location = "{\"x\":2,\"y\":3}"
-    GudgeonPond Location = "{\"x\":4,\"y\":2}"
+
+type Location struct{
+    XPos int `json:"x"`
+    YPos int `json:"y"`
+}
+
+var (
+Spawn Location = Location{XPos:0, YPos:0}
+Chicken Location = Location{XPos:0, YPos:1}
+CopperMine Location = Location{XPos:2, YPos:0}
+IronMine Location = Location{XPos:1, YPos:7}
+CoalMine Location = Location{XPos:1, YPos:6}
+Forge Location = Location{XPos:1, YPos:5}
+WeaponSmith Location = Location{XPos:2, YPos:1}
+Bank Location = Location{XPos:4, YPos:1}
+AshWood Location = Location{XPos:-1, YPos:0}
+SpruceWood Location = Location{XPos:1, YPos:9}
+BirchWood Location = Location{XPos:-1, YPos:6}
+Sawmill Location = Location{XPos:-2, YPos:-3}
+YellowSlime Location = Location{XPos:4, YPos:-1}
+BlueSlime Location = Location{XPos:2, YPos:-1}
+RedSlime Location = Location{XPos:1, YPos:-1}
+Mushmush Location = Location{XPos:5, YPos:3}
+MonsterTask Location = Location{XPos:1, YPos:2}
+Sunflower Location = Location{XPos:2, YPos:2}
+Alchemist Location = Location{XPos:2, YPos:3}
+GudgeonPond Location = Location{XPos:4, YPos:2}
 )
+
+func (l Location) String() string {
+    return fmt.Sprintf("{\"x\":%d,\"y\":%d}", l.XPos, l.YPos)
+}
 
 type ToonName string
 const (
@@ -57,10 +68,7 @@ type Monster struct {
 }
 
 type MapLocation struct {
-    Data []struct{
-        XPos int `json:"x"`
-        YPos int `json:"y"`
-    } `json:"data"`
+    Data []Location `json:"data"`
 }
 
 type Toon struct {
@@ -96,7 +104,7 @@ type ToonDetails struct {
 func artifactsMove(td ToonDetails, BODY Location) []byte {
     // TO-DO: Don't Try To Move If Already There
     p := "my/"+string(td.Name)+"/action/move";
-    return artifactsRest("POST", p, string(BODY));
+    return artifactsRest("POST", p, BODY.String());
 };
 
 func artifactsPost(PATH string, BODY string) []byte {
@@ -148,7 +156,7 @@ func BankDeposit(ToonName ToonName) {
             artifactsPost("my/"+string(ToonName)+"/action/bank/deposit","{\"code\":\""+td.Inventory[i].Code+"\",\"quantity\":"+strconv.Itoa(td.Inventory[i].Quantity)+"}");
         }
     }
-    artifactsMove(td, Location("{\"x\":"+strconv.Itoa(td.XPos)+",\"y\":"+strconv.Itoa(td.YPos)+"}")); 
+    artifactsMove(td, Location{XPos:td.XPos, YPos:td.YPos}); 
 
 }
 
@@ -256,7 +264,7 @@ func RunMonsterTasks(ToonName ToonName) {
             json.Unmarshal(monsterLocation, &mapLoc);
             logger.Debug("monsterLocation:", string(monsterLocation));
             logger.Debug(mapLoc);
-            FightThe(Location("{\"x\":"+strconv.Itoa(mapLoc.Data[0].XPos)+",\"y\":"+strconv.Itoa(mapLoc.Data[0].YPos)+"}"), ToonName, -1); 
+            FightThe(Location{XPos: mapLoc.Data[0].XPos,YPos:mapLoc.Data[0].YPos}, ToonName, -1); 
         }
     }
 
@@ -279,6 +287,10 @@ func EnsureOffCooldown(Toons Toon, ToonName ToonName) {
 }
 
 func main() {
+    EnvErr := godotenv.Load()
+    if EnvErr != nil {
+       logger.Fatal("Error loading .env file")
+    }
     logger.Info("Troy's Artifacts Runner")
     os.Getenv("ARTIFACTS_API_KEY")
     myToons := artifactsGet("my/characters");
